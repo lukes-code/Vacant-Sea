5db8f5cef377b6a52f504bdf5eeb9bc553cbc1cc
Luke Sterry <lukesterry@gmail.com>
2023-08-11 17:13:28 +0100

    theme choice in local storage

--- src/context/theme.tsx
+++ src/context/theme.tsx
@@ -5,7 +5,7 @@ const ThemeContext = createContext<any>(undefined);
 type ThemeContextType = {
   darkMode: boolean;
   setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
-}
+};
 
 type ThemeProviderProps = {
   children: React.ReactNode;
@@ -13,27 +13,35 @@ type ThemeProviderProps = {
 
 export const useThemeContext = () => {
   const context = useContext(ThemeContext);
-  if (!context) {
+  if (!context)
     throw new Error("useThemeContext must be used within a ThemeProvider");
-  }
   return context;
 };
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
-  const [darkMode, setDarkMode] = useState<boolean>(true);
+  const [darkMode, setDarkMode] = useState<boolean>(() => {
+    if (typeof window !== "undefined") {
+      const storedDarkMode = localStorage.getItem("darkMode");
+      return storedDarkMode ? JSON.parse(storedDarkMode) : true;
+    }
+    return true; // Default value for server-side rendering
+  });
 
   useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
+    if (typeof window !== "undefined") {
+      localStorage.setItem("darkMode", JSON.stringify(darkMode));
+      darkMode
+        ? document.documentElement.classList.add("dark")
+        : document.documentElement.classList.remove("dark");
     }
   }, [darkMode]);
 
   const value: ThemeContextType = {
     darkMode,
-    setDarkMode
+    setDarkMode,
   };
 
-  return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>;
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
 };
